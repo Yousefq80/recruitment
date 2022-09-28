062f7bd56ffe6e2a67cbf69f761e22c0dd0fbbb1
Yousefq80 <114352485+Yousefq80@users.noreply.github.com>
2022-09-28 03:07:38 +0300

    all functions are done except (acceptance function)

--- recruitment.py
+++ recruitment.py
@@ -1,21 +1,10 @@
 # This function returns a list of skills.
 # This is the list that the user will choose from
 # Add at least 3 random skills for the user to select from
-skills={
-    "skill_show":[
-        {"sk":"Java","id":1},
-        {"sk":"python","id":2},
-        {"sk":"C++","id":3}
-             ]
-          }
+skills = ["Python", "C++", "Javascript", "Juggling", "Running", "Eating"]
+cv= {}
 def get_skills():
-    skills={
-    "skill_show":[
-        {"sk":"Java","id":1},
-        {"sk":"python","id":2},
-        {"sk":"C++","id":3}
-             ]
-          }
+    skills = ["Python", "C++", "Javascript", "Juggling", "Running", "Eating"]
     return skills
 
 
@@ -25,10 +14,9 @@ def get_skills():
 # It takes the list of skills as an argument and prints them numbered
 # This function doesn't return anything
 def show_skills(skills):
-    print("Skills:\n")
-    for skill in skills:
-        for skil in skills["skills_show"]:
-         print(skil.get("id"),skil.get("sk"))
+    for x, element in enumerate(skills):
+     print(str(x+1)+"-",element)
+
 
 
 # Shows the available skills and have user pick from them two skills
@@ -36,43 +24,49 @@ def show_skills(skills):
 # For example, if the user enters 1, the first skill in your list of skills will be added to the list
 # Return a list of the two skills that the user inputted
 def get_user_skills(skills):
-    userskill=[]
+  userskill=[] 
+  n =len(skills)  
+  usk1=int(input("please choose a skill by entring number form the list:"))
+  usk2=int(input("please choose another skill by entring number form the list:"))
+  for x in range(n):
+        
+    if (x+1) == usk1:
+      userskill.append(skills[x])
+    if (x+1) == usk2:
+      userskill.append(skills[x])
+        # else:
+        #     usk2=int("you have selected this number before please enter another number")
+  return userskill
     
-    print(show_skills(skills))
-    for x in range(2):
-        userskill=input("please choose:")
-    return userskill
- 
-
 
 # This function will get the user's cv from their inputs
 # HINT: Use previous built functions to get the skills from the user
 def get_user_cv(skills):
-      skil= {"name":"","age":"","experience":"",
-       "skills":[]
-         }
-
-      username = input("please enter your name:")
-      userage= input("please ener your age:")
-      exp=input("please enter how many experience you have:")
-      skil["name"]=username
-      skil["age"]=userage
-      skil["experience"]=exp
+      cv= {}
+      name=input("please enter your name:")
+      cv["name"] = name
+      age=input("please enter your age:")
+      cv["age"]= age 
+      exp=int(input("please enter how many yesrs of experience you have:"))
+      cv["experience"]=exp 
       show_skills(skills)
-      get_user_skills(skills)
-      
+      cv["skills"]=[]
+       
+      cv["skills"].append(get_user_skills(skills))
+      print(cv) 
 
 # This functions checks if the cv is acceptable or not, by checking the age, experience and skills and return a boolean (True or False) based on that
 def check_acceptance(cv, desired_skill):
-    ...
+  ...
 
 
 def main():
     # Write your main logic here by combining the functions above into the
     # desired outcome
+    print("Welcome to the recruitment program, please answer the following questions:")
     get_user_cv(skills)
-
+    
+  #   print(f"you have been accepted,{cv.get("name")")
 
 if __name__ == "__main__":
     main()
-
